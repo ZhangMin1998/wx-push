bd63023f129278d2d64f9a40219698580d0d7dbd
ZhangMin1998 <66857222+ZhangMin1998@users.noreply.github.com>
2022-12-17 02:02:12 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,8 @@ module.exports = {
   appid: "wx8c850259e71d2a9e",
   appsecret: "7ed56a3ca07f1058d28ac6801945e7e0",
   template_id: "M8NpvPxcAVU0rhKHKH7oPE9UyjC9O6s7hbqkhjkz9Wk",
-  touser: "o1yOL6EwYFQpuf3XtxIw9OQBDXO0",
+//   touser: "o1yOL6EwYFQpuf3XtxIw9OQBDXO0", // 杨老师
+  touser: "o1yOL6Lqu72mY8y77IcAefogO6QM", // 张敏
 
   key: "d3c9a48aff2792627fb04b2407cbf189",
   city: "深圳",
