0b47d91b87c990d7591908f0edd2af45ec952263
ZhangMin1998 <66857222+ZhangMin1998@users.noreply.github.com>
2022-12-17 00:58:40 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wx8c850259e71d2a9e",
   appsecret: "7ed56a3ca07f1058d28ac6801945e7e0",
   template_id: "M8NpvPxcAVU0rhKHKH7oPE9UyjC9O6s7hbqkhjkz9Wk",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  touser: "o1yOL6EwYFQpuf3XtxIw9OQBDXO0",
 
   key: "d3c9a48aff2792627fb04b2407cbf189",
   city: "深圳",
